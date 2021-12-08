2939a5f736283f4aaeb1675db916f63913f898df
Ashish <ashshshanu96@gmail.com>
2021-12-08 09:55:33 +0530

    initial commit

---
+++ databaseconnection.py
@@ -0,0 +1,37 @@
+import psycopg2
+from flask_pymongo import MongoClient
+from psycopg2 import Error, DataError
+from configuration import Configuration
+
+Configuration.init_config('config.ini')
+
+
+class DatabaseConnection:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_connection(section):
+        host = Configuration.get(section, 'host')
+        port = Configuration.get(section, 'port')
+        db_name = Configuration.get(section, 'db_name')
+        user = Configuration.get(section, 'user')
+        password = Configuration.get(section, 'password')
+        try:
+            connection = psycopg2.connect(user=user, password=password, host=host, port=port,
+                                          database=db_name)
+            return connection
+        except (Exception, Error, DataError) as error:
+            print('Error while connecting to PostgreSQL', error)
+
+
+
+def mongodb_connection(key, section_mongodb):
+    connection_string = "mongodb://grey:greywolf@101.53.132.174:5435/catax?authSource=catax&readPreference=primary" \
+                        "&appname=MongoDB%20Compass&directConnection=true&ssl=false "
+    client = MongoClient(connection_string)
+    db = client.get_database(Configuration.get(section_mongodb, 'dbname'))
+
+    collection = db.get_collection(Configuration.get(section_mongodb, 'collection_name'))
+    cursor = collection.find_one({"apiKey": key})
+    return cursor
---
+++ main.py
@@ -0,0 +1,53 @@
+import datetime
+import json
+
+import psycopg2
+from flask import Flask, request, make_response
+from pymongo import MongoClient
+
+from databaseconnection import DatabaseConnection, mongodb_connection
+
+app = Flask(__name__)
+
+
+@app.route('/post', methods=['POST'])
+def internal_authentication():
+    key = request.headers.get('API-Key')
+
+    cursor = mongodb_connection(key, "mongodb")
+
+    if cursor is not None and cursor["apiKey"] == key:
+        if cursor["expiry"] > datetime.datetime.now():
+            userid = cursor["userId"]
+            accountid = cursor["accountId"]
+
+            json_body = dict(request.json)
+
+            try:
+                connection = DatabaseConnection.get_connection('database')
+                cursor_postgres = connection.cursor()
+                cursor_postgres.execute(
+                    "insert into transaction_push (user_id, exchange_id, transaction_json, date) values (" + "'" + str(
+                        userid) + "',"
+                                  "'" + str(accountid) + "','" + json.dumps(json_body) + "','" + str(
+                        datetime.datetime.now()) + "') ON CONFLICT DO NOTHING")
+                connection.commit()
+
+                if connection:
+                    connection.close()
+
+                return make_response('Transaction stored', 201, {"response": "Success"})
+            except psycopg2.Error as db_err:
+                print('Exception occurred {}', db_err.__str__())
+                return make_response('Error inserting data (Postgres): ', 500, {'response': 'Invalid token!'})
+            except ConnectionError as err:
+                print('Exception occurred {}', err.__str__())
+                return make_response('Error connecting Postgres:', 500, {'response': 'Invalid token!'})
+        else:
+            return make_response('Token expired:', 401, {'response': 'Token expired'})
+
+    return make_response('Unauthorized', 401, {'response': 'Unauthorized'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
